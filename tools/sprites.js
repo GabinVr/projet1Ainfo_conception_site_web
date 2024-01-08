ff8ceafc5ea4c930b6d6a1a3fe011c8c846ee336
GabinVr <147336973+GabinVr@users.noreply.github.com>
2024-01-08 10:00:41 +0000

    Anacée sprite.js

--- tools/sprites.js
+++ tools/sprites.js
@@ -15,6 +15,21 @@ function drawCanvasImage(image,posX,posY)
 {
 
 }
+
+
+
+// ===================================================================================
+// Definition de l'objet CanvasSprite
+let CanvasSprite = {
+    animations : [],
+    currentAnimation : [],
+    currentTile : 0,
+    loop : false,
+}
+// ===================================================================================
+// Definition de l'objet Animation
+let Animation = {}
+
 // ===================================================================================
 // Constructeur for an animation object
 // image: graphics source
@@ -23,9 +38,16 @@ function drawCanvasImage(image,posX,posY)
 // nbXTiles : nombre de tiles horizontalement
 // nbYTiles : nombre de tiles verticallement
 // loop : animation en boucle (true) ou non (false)
-function CanvasSprite(spriteImgURL, x, y, widthTile, heightTile, nbXTiles, nbYTiles,ctx)
+function CanvasSprite(spriteImgURL, x, y, widthTile, heightTile, nbXTiles, nbYTiles, ctx)
 {
-   
+    this.image = canvasImage(spriteImgURL, x, y, ctx);
+    this.x = x;
+    this.y = y;
+    this.width = widthTile;
+    this.height = heightTile;
+    this.nbXTiles = nbXTiles;
+    this.nbYTiles = nbYTiles;
+    this.loop = false;
 }
 // -----------------------------------------------------------------------------------
 // Ajout d'une animation spécifique
@@ -33,12 +55,24 @@ function CanvasSprite(spriteImgURL, x, y, widthTile, heightTile, nbXTiles, nbYTi
 // tiles : tableau d'indices de tile
 CanvasSprite.prototype.addAnimation = function(nameAnim, tiles)
 {
+    animation = new Animation;
+    animation.name = nameAnim;
+    animation.tiles = tiles;
+    if (this.animations && Array.isArray(this.animations)) {
+        this.animations.push(animation);
+    }
     
 }
 // -----------------------------------------------------------------------------------
 // Sélectionne une animation spécifique nameAnim
 CanvasSprite.prototype.selectAnimation = function(nameAnim,loop)
 {
+
+    for (let animation of this.animations) {
+        if (animation.name === nameAnim) {
+
+        }
+    }
     
 }
 // -----------------------------------------------------------------------------------
@@ -52,25 +86,29 @@ CanvasSprite.prototype.nextTile = function()
 // Retourne la position de la tile dans le sprite selon x
 CanvasSprite.prototype.tileX = function(tileIndex)
 {
-	
+	return this.width*(tileIndex+1);
 }
 // -----------------------------------------------------------------------------------
 // Retourne la position de la tile dans le sprite selon y
 CanvasSprite.prototype.tileY = function(tileIndex)
 {
-	
+	return this.height*(tileIndex+1);
 }
 // -----------------------------------------------------------------------------------
 // Dessine une tile
 CanvasSprite.prototype.drawTile = function(tileIndex)
 {
-    
+    drawCanvasImage(this.image, this.tileX, this.tileY);   
 };
 // ----------------------------------------------------------------------------------
 // Dessine une tile
 CanvasSprite.prototype.simpleAnim = function(tps)
 {
-	
+    
+	while (this.loop) {
+        this.nextTile
+
+    }
 }
 // ----------------------------------------------------------------------------------
 CanvasSprite.prototype.stopAnim = function()
